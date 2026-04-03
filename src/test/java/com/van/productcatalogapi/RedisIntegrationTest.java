8e2229ec7c712e5ca698923ff6808c8afb0e66aa
vanessacardosodsantos <vanessacardoso.santos@gmail.com>
2026-04-03 20:29:05 -0300

    fix: Alrrige contexto do SpringBootTest nos testes de integração do Redis

--- src/test/java/com/van/productcatalogapi/RedisIntegrationTest.java
+++ src/test/java/com/van/productcatalogapi/RedisIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
@@ -17,7 +18,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(classes = RedisAutoConfiguration.class)
 @Testcontainers
 @EnableAutoConfiguration(exclude = {
         DataSourceAutoConfiguration.class,
